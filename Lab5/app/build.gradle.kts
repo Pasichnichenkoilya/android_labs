1686f7540a56e5f368ce73d823879886e48856a2
Pasichnichenkoilya <sheredan2003@gmail.com>
2024-05-18 17:54:40 +0300

    fix

--- Lab5/app/build.gradle.kts
+++ Lab5/app/build.gradle.kts
@@ -1,14 +1,19 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.example.lab5"
+    namespace = "com.example.lab_5"
     compileSdk = 34
 
+    viewBinding {
+        enable = true
+    }
+
+
     defaultConfig {
-        applicationId = "com.example.lab5"
+        applicationId = "com.example.lab_5"
         minSdk = 25
         targetSdk = 34
         versionCode = 1
@@ -37,12 +42,11 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- Lab5/settings.gradle.kts
+++ Lab5/settings.gradle.kts
@@ -1,12 +1,6 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -19,5 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Lab5"
+rootProject.name = "Lab_5"
 include(":app")
+ 
